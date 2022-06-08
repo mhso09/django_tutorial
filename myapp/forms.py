9e09e5f5533c5da9daea78c62e7fff5ff971c5ea
mhso09 <mhso0@naver.com>
2022-06-08 18:36:00 +0900

    변경

--- myapp/forms.py
+++ myapp/forms.py
@@ -6,7 +6,11 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question # 사용할 모델
         fields = ["subject", "content"] # QuestionForm에서 사용할 Question 모델 속성
-        
+
+        label = {
+            'subject' : '제목',
+            'content': '내용',
+        }
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
